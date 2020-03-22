ae50b76dbd0fd63ad3c45c9b1b9474a79e38e0a3
Ryan B. Harvey <ryan.b.harvey@ted.com>
2020-03-22 03:38:14 -0500

    Add tests for src/lib/rando's range function too

--- src/lib/rando.test.ts
+++ src/lib/rando.test.ts
@@ -1,4 +1,4 @@
-import { getShrines } from "./rando";
+import { range, getShrines } from "./rando";
 const plateauShrines = [38, 41, 9, 65];
 const bloodMoonShrine = 78;
 const eventideShrine = 97;
@@ -13,6 +13,52 @@ const getRandomSeed = () => {
 }
 
 describe('rando', () => {
+  describe('range', () => {
+    describe('when a limit is provided', () => {
+      it('provides an array of the expected length', () => {
+        const vals = range(10, 20);
+        expect(vals.length).toEqual(11);
+      });
+      it('provides sequential numbers', () => {
+        const vals = range(10, 20);
+        vals.reduce((prev, curr, _idx, _arr) => {
+          expect(prev).toEqual(curr - 1);
+          return curr;
+        });
+      });
+      it('provides values starting with the lower bound', () => {
+        const vals = range(10, 20);
+        expect(vals[0]).toEqual(10);
+      });
+      it('provides values ending at the upper bound', () => {
+        const vals = range(10, 20);
+        expect(vals[vals.length - 1]).toEqual(20);
+      });
+    });
+
+    describe('when a limit is not provided', () => {
+      it('provides an array of the expected length', () => {
+        const vals = range(10);
+        expect(vals.length).toEqual(11);
+      });
+      it('provides sequential numbers', () => {
+        const vals = range(10);
+        vals.reduce((prev, curr, _idx, _arr) => {
+          expect(prev).toEqual(curr - 1);
+          return curr;
+        });
+      });
+      it('provides values starting with zero', () => {
+        const vals = range(10);
+        expect(vals[0]).toEqual(0);
+      });
+      it('provides values ending at the bound provided', () => {
+        const vals = range(10);
+        expect(vals[vals.length - 1]).toEqual(10);
+      });
+    });
+  });
+
   describe('getShrines', () => {
     it('shuffles the shrines', () => {
       for (let n = 0; n < 10; n++) {
