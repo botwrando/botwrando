90552795a6b98328a88c8b3fa081fd265319f666
Johan Althoff <teetow@gmail.com>
2020-03-27 20:42:24 +0100

    fix  Quit button visibility issues

--- src/components/AppHeader/AppHeader.test.tsx
+++ src/components/AppHeader/AppHeader.test.tsx
@@ -11,28 +11,35 @@ describe('AppHeader', () => {
   });
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<AppHeader setRun={setRun} />, div);
+    ReactDOM.render(<AppHeader hasSeed={true} setRun={setRun} />, div);
   });
-  it('renders the header contents', () => {
+  it('renders the Quit button when a seed has been picked', () => {
+    const el = shallow(<AppHeader hasSeed={true} setRun={setRun} />);
+    expect(el.contains(<div className="header" />));
+    expect(el.contains(<div className="btn-back" />));
+  });
+  it('renders the seed picker screen without the Quit button', () => {
     const el = shallow(<AppHeader setRun={setRun} />);
-    expect(el.contains(<div className="header"/>));
+    expect(el.contains(<div className="header" />));
+    expect(el.contains(<div className="btn-back" />)).toBe(false);
   });
   it('mounts in a full DOM', () => {
     expect(mount(<AppHeader setRun={setRun} />).find('.header').length).toBe(1);
   });
   it('should render the expected text', () => {
-    expect(render(<AppHeader setRun={setRun} />).text())
-      .toMatch(/BotW All Shrines Randomizer/);
+    expect(render(<AppHeader setRun={setRun} />).text()).toMatch(
+      /BotW All Shrines Randomizer/
+    );
   });
   describe('when the Quit Run button is clicked', () => {
     it('should call setRun', () => {
-      const el = mount(<AppHeader setRun={setRun} />);
+      const el = mount(<AppHeader hasSeed={true} setRun={setRun} />);
       el.find('button').simulate('click');
       expect(setRun).toHaveBeenCalledTimes(1);
     });
     it('should set the default run', () => {
       const defaultRun = getDefaultRun();
-      const el = mount(<AppHeader setRun={setRun} />);
+      const el = mount(<AppHeader hasSeed={true} setRun={setRun} />);
       el.find('button').simulate('click');
       expect(setRun).toHaveBeenCalledWith(defaultRun);
     });
--- src/components/RunManager/RunManager.tsx
+++ src/components/RunManager/RunManager.tsx
@@ -178,7 +178,7 @@ export const RunManager = (props: RunManagerProps) => {
     <div className={getClasses()}>
       <KeyboardEventHandler handleKeys={['all']} onKeyEvent={handleKey} />
       <div className="main">
-        <AppHeader setRun={setRun} />
+        <AppHeader hasSeed={!!run.seed} setRun={setRun} />
         <SeedInfo run={run} />
         {mainsection()}
         <AppFooter
