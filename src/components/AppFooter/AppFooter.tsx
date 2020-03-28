1ea9065f558e4780f58bdcc3ea71491ec2ea3b20
Johan Althoff <teetow@gmail.com>
2020-03-29 00:44:08 +0100

    improve mobile layout

--- src/components/AppFooter/AppFooter.tsx
+++ src/components/AppFooter/AppFooter.tsx
@@ -3,33 +3,35 @@ import { Run } from '../../lib/run';
 import { DesktopHelp, MobileControls } from '../Help/Help';
 
 export type AppFooterProps = {
-  run: Run,
+  run: Run;
   touchCallbacks: {
-    onSplit: (event: React.MouseEvent) => void
-    onUndo: (event: React.MouseEvent) => void
-    onReset: (event: React.MouseEvent) => void
-    onPause: (event: React.MouseEvent) => void
-    onBloodMoon: (event: React.MouseEvent) => void
-  },
-  showHelp: boolean
-}
+    onSplit: (event: React.MouseEvent) => void;
+    onUndo: (event: React.MouseEvent) => void;
+    onReset: (event: React.MouseEvent) => void;
+    onPause: (event: React.MouseEvent) => void;
+    onBloodMoon: (event: React.MouseEvent) => void;
+  };
+  showHelp: boolean;
+};
 
-export function AppFooter ({ run, touchCallbacks, showHelp }: AppFooterProps) {
+export function AppFooter({ run, touchCallbacks, showHelp }: AppFooterProps) {
   const isTouch = window.matchMedia('(pointer: coarse)').matches;
-  return (
+  return run.seed != '' ? (
     <div className="footer">
-      {isTouch?(
+      {isTouch ? (
         <MobileControls
-          run = { run }
-          onSplit = { touchCallbacks.onSplit }
-          onUndo = { touchCallbacks.onUndo }
-          onReset = { touchCallbacks.onReset }
-          onPause = { touchCallbacks.onPause }
-          onBloodMoon = { touchCallbacks.onBloodMoon }
+          run={run}
+          onSplit={touchCallbacks.onSplit}
+          onUndo={touchCallbacks.onUndo}
+          onReset={touchCallbacks.onReset}
+          onPause={touchCallbacks.onPause}
+          onBloodMoon={touchCallbacks.onBloodMoon}
         />
-      ): (
-        <DesktopHelp run = { run } showHelp = { showHelp } />
+      ) : (
+        <DesktopHelp run={run} showHelp={showHelp} />
       )}
     </div>
+  ) : (
+    <></>
   );
 }
