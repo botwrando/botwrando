cf2449ea6bd07ec519823bb4769ea81f51641e30
Johan Althoff <teetow@gmail.com>
2020-05-22 23:44:55 +0200

    hide seed id string by default, add toggle control

--- src/components/SeedInfo/SeedInfo.tsx
+++ src/components/SeedInfo/SeedInfo.tsx
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Run } from '../../lib/run';
+import './SeedInfo.scss';
 
 export type SeedInfoProps = {
   run: Run
 }
 
 export function SeedInfo({ run }: SeedInfoProps) {
+  const [showSeed, setShowSeed] = useState(false);
+
+  const toggleSeed = (isVisible: boolean) => (setShowSeed(isVisible));
+
+  const seedViewer = () => (
+    (<div className='seed'>
+      <span className='seednumber'>Seed: {run.seed} </span>
+      <span className='toggle' onClick={() => toggleSeed(false)}> Hide </span>
+    </div>)
+  )
+
   return (
-    <div className="seedinfo">
-      { run.seed ? (<div className="seed">Seed: {run.seed}</div>) : <></>}
+    <div className='seedinfo'>
+      {run.seed ?
+
+        showSeed ?
+          seedViewer() :
+          (
+            <div className='seed'>
+              <div className='toggle' onClick={() => toggleSeed(true)}> Show seed </div>
+            </div>
+          ) :
+        (<></>)
+      }
     </div>
   );
 }
