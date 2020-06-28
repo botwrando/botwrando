d211be8bf33e4f841ec055b4153e280eb2ebcc58
Ryan B. Harvey <ryan.b.harvey@gmail.com>
2020-06-28 02:17:36 -0500

    Add DistanceExplorer for development use, stubbed into App

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App type="default" />, document.getElementById('root'));
+// ReactDOM.render(<App type="distances" />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
